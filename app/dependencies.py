1fc4e35199c633883d9575419e8d63beec74ff3b
Zakka24 <aoukaili.zakaria@gmail.com>
2026-01-27 11:00:47 +0100

    use gemini-3-pro al LLM

--- app/dependencies.py
+++ app/dependencies.py
@@ -18,7 +18,7 @@ def get_model_instance():
 
     return Model(
         embeddings_model="qwen3-embedding:0.6b",
-        chat_model='gemini-2.5-pro',
+        chat_model='gemini-3-pro-preview',
         api_key = os.getenv("GEMINI_API_KEY"),
         base_url=base_url
     )
